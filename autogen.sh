abdb90a600e91215bf26daa8ca28fdf2ba6048da
Emmanuele Bassi <ebassi@gnome.org>
2010-09-28 00:17:06 +0100

    build: Remove shave
    Use Automake 1.11 and its silent rules.

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-REQUIRED_AUTOMAKE_VERSION=1.7
+REQUIRED_AUTOMAKE_VERSION=1.11
 PKG_NAME="GNOME Utilities"
 
 (test -f $srcdir/configure.ac \
@@ -22,12 +22,3 @@ which gnome-autogen.sh || {
 }
 
 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
-
-# we need to patch gtk-doc.make to support pretty output with
-# libtool 1.x.  Should be fixed in the next version of gtk-doc.
-# To be more resilient with the various versions of gtk-doc one
-# can find, just sed gkt-doc.make rather than patch it.
-sed -e 's#) --mode=compile#) --tag=CC --mode=compile#' gtk-doc.make > gtk-doc.temp \
-                && mv gtk-doc.temp gtk-doc.make
-sed -e 's#) --mode=link#) --tag=CC --mode=link#' gtk-doc.make > gtk-doc.temp \
-                && mv gtk-doc.temp gtk-doc.make
